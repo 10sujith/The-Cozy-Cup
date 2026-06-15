f89d4824a91db76cde7ca83ebc7224ea23e83ea4
Sujith K <155721257+10sujith@users.noreply.github.com>
2026-06-15 15:25:49 +0530

    final commit

---
+++ Coffee.js
@@ -0,0 +1,58 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+// const navMenu = document.querySelector(".nav-menu");
+
+menuOpenButton.addEventListener("click", () => { 
+    // Toggle the visibility of the mobile menu
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+// Close the mobile menu when the close button is clicked
+menuCloseButton.addEventListener("click", () => menuOpenButton.click()); 
+
+// Close the mobile menu when the nav link is clicked
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => menuOpenButton.click());
+});
+
+// Initialize Swiper
+
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  garbCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets : true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+// Responsive breakpoints
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      // spaceBetween: 20,
+    },
+
+    768: {
+      slidesPerView: 2,
+      // spaceBetween: 20,
+    },
+
+    1024: {
+      slidesPerView: 3,
+      // spaceBetween: 20,
+    },
+  },
+
+});
